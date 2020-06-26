0deeee7ad1b1b8e97c3805f36ca9f1fca3518027
calvin adeniyi <lordcalvin7@gmail.com>
2020-06-26 15:02:13 +0100

    initial commit

---
+++ build/precache-manifest.bad3e5d065eac258db306121003344c3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13a11d41b3136b105d73fa81d07351f2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ce3c6785f0965f45de8f",
+    "url": "/static/css/3.c473469c.chunk.css"
+  },
+  {
+    "revision": "a6dfcd78676ff874f377",
+    "url": "/static/css/main.a43c9bf9.chunk.css"
+  },
+  {
+    "revision": "8dbcc18b44ff23f02fa8",
+    "url": "/static/js/2.555e621f.chunk.js"
+  },
+  {
+    "revision": "097d3e93848f2039990ec8db3ab0a617",
+    "url": "/static/js/2.555e621f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce3c6785f0965f45de8f",
+    "url": "/static/js/3.8be3ad2d.chunk.js"
+  },
+  {
+    "revision": "a6dfcd78676ff874f377",
+    "url": "/static/js/main.d5d7ccff.chunk.js"
+  },
+  {
+    "revision": "500620f1456fa878ada7",
+    "url": "/static/js/runtime-main.bce0307e.js"
+  }
+]);
